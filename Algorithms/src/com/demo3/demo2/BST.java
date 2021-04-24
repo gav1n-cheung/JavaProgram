6ca86ebdfbf18eb2380b79f73cf800c419e4a0d4
gav1n-cheung <gavinbr@outlook.com>
2021-04-24 17:35:27 +0800

    4.24提交

--- Algorithms/src/com/demo3/demo2/BST.java
+++ Algorithms/src/com/demo3/demo2/BST.java
@@ -3,6 +3,8 @@ package com.demo3.demo2;
 import com.demo.demo3.ex1319;
 import org.w3c.dom.Node;
 
+import java.util.logging.Level;
+
 /**
  * @author Cheung
  * @date 2021/4/23 9:27
@@ -22,6 +24,7 @@ public class BST<Key extends Comparable<Key>,Value> {
     }
     private int size(){
         return size(root);
+
     }
     private int size(Node root){
         if (root==null) return 0;
@@ -45,6 +48,29 @@ public class BST<Key extends Comparable<Key>,Value> {
         x.N=size(x.left)+size(x.right)+1;
         return x;
     }
+    public Key min(){
+        if (root==null) return null;
+        return min(root).key;
+    }
+    private Node min(Node minNode){
+        if (minNode.left ==null) return minNode;
+        return min(minNode.left);
+    }
+    public Key floor(Key key){
+        Node x=floor(root,key);
+         if (x==null) return null;
+         return x.key;
+    }
+    private Node floor(Node x,Key key){
+        if (x ==null) return null;
+        int cmp=key.compareTo(x.key);
+        if (cmp==0) return x;
+        if (cmp<0) floor(x.left,key);
+        Node t=floor(x.right,key);
+        if (t!=null) return t;
+        else return x;
+    }
+
 
 
 }
